4900ae739c287e0c6f42b7ce7205de44e54ec0c4
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-07-21 20:34:22 -0700

    Delete twonumbers.py

--- twonumbers.py
+++
@@ -1,6 +0,0 @@
-firstnum, secondnum = int(input("First number: ")), int(input("Second number: "))
-print("The sum of the two numbers is: " + str(firstnum + secondnum))
-
-firstnum = int(input("First number: "))
-secondnum = int(input("Second number: "))
-print("The sum of the two numbers is: " + str(firstnum+secondnum))
